e39405aadacd87efbc3c1864eedeb620c89b85e5
thelastligma <thelastligma420@gmail.com>
2026-01-08 14:27:27 -0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,10 +11,10 @@ echo "===================="
 
 # 1. Detect Architecture
 ARCH=$(uname -m)
-if [[ "$ARCH" == "silicon" ]]; then
+if [[ "$ARCH" == "silicon-arm64" ]]; then
   FILE_NAME="Arcadia-silicon.zip"
   echo "Detected: Apple Silicon ($ARCH)"
-elif [[ "$ARCH" == "intel" ]]; then
+elif [[ "$ARCH" == "intel-x64" ]]; then
   FILE_NAME="Arcadia-intel.zip"
   echo "Detected: Intel ($ARCH)"
 else
